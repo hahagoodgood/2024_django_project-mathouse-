6c06abd027c46c13a5bae6c79c19e2c775e2411d
DHKim <dh95229505@gmail.com>
2024-03-03 12:03:52 +0900

    feat: ORM을 통해 restaurant 모델 생성

---
+++ restaurant/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Restaurnt(models.Model):
+    # 이름
+    name = models.CharField(max_length = 50)
+    # 주소
+    address = models.CharField(max_length = 255)
+    # 전화번호
+    phone = models.CharField(max_length = 20)
+    # 위도
+    latitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    # 경도
+    longitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    # 점수
+    rating = models.SmallIntegerField()
+    # 생성 시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 업데이트
+    updated_at = models.DateTimeField(auto_now=True)
